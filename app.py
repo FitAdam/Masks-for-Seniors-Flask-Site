0a99b91b937928504499b8a2f23456fad72c6f94
F1tVdvm <adam.george617@gmail.com>
2020-04-22 20:10:19 +0100

    Complete registration form

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, redirect, render_template, request, session, flash, url_for
 from forms import RegistrationForm
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm
+
 app = Flask(__name__)
 
-orders=[{
-    'name': 'Ellon',
-    'address': 'Sydney'
-}]
+
 
 app.config['SECRET_KEY'] = 'b0c363e1be51668250911243a6b1c167'
 
@@ -13,18 +13,19 @@ app.config['SECRET_KEY'] = 'b0c363e1be51668250911243a6b1c167'
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html", orders=orders)
+    return render_template("home.html", )
 
 @app.route("/register", methods=["GET","POST"])
 def register():
         form = RegistrationForm()
         if form.validate_on_submit():
+            print(f'We are sending masks for {form.name.data}')
             return redirect(url_for('confirm'))
         return render_template('formularz.html',title='Register',form=form)
 
 @app.route("/potwierdzenie")
 def confirm():
-    return render_template('potwierdzenie.html',orders=orders)
+    return render_template('potwierdzenie.html')
     
 
 
